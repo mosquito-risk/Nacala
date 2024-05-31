ddf8e9b8f3b610f00d20f0d876ac3b9f5b2c9624
Hui Zhang <omegazhanghui@gmail.com>
2024-05-31 13:16:20 +0200

    update abstract

--- docs/js/abstract.js
+++ docs/js/abstract.js
@@ -7,23 +7,24 @@ const Abstract = () => {
                         <h2 class="title is-3">Abstract</h2>
                         <div class="content has-text-justified">
                             <p>
-                                Low-quality housing is associated with an increased risk of mosquito-borne diseases. As certain roof characteristics are linked to an elevated risk of malaria,
-                                classification of roof types based on remote sensing imagery can support assess-
-                                ment of malaria risk and help preventing the disease. To support research in this
+                                Low-quality housing is associated with an increased risk of mosquito-borne diseases.
+                                As certain roof characteristics are linked to an elevated risk of malaria,
+                                classification of roof types based on remote sensing imagery can support assessment 
+                                of malaria risk and help preventing the disease. To support research in this
                                 area, we release the Nacala-Roof-Material dataset, which contains drone images
                                 from Mozambique with corresponding labels delineating houses and specifying
-                                their roof types. The dataset defines a multi-task computer vision problem, comprising object detection, classification, and segmentation. In addition, we benchmarked
+                                their roof types. The dataset defines a multi-task computer vision problem, comprising 
+                                object detection, classification, and segmentation. In addition, we benchmarked
                                 different state-of-the-art approaches on the dataset. Canonical U-Nets, YOLOv8,
                                 and a custom decoder on pretrained DINOv2 served as baselines. We show that
                                 each of the methods has its advantages but none is superior on all tasks. While the
                                 tasks are closely related, accurate segmentation of objects need not imply accurate
-                                instance separation, and vice versa. This causes problems for methods designed
-                                for pixel-level semantic segmentation such as the standard U-Net architecture. We
-                                address this general issue by introducing a variant of the deep ordinal watershed
-                                (DOW) approach, where several output maps are generated allowing for accu-
-                                rate object delineation and separation. This improved the performance of both
-                                the U-Net architecture and our systems based on segmentations using DINOv2
-                                features.
+                                instance separation, and vice versa. We address this general issue by introducing a
+                                variant of the deep ordinal watershed (DOW) approach, where several output maps
+                                are generated allowing for accurate object delineation and separation. We show
+                                that DOW is a generic approach that improves the performance of both U-Net and
+                                DINOv2 backbones leading to a better trade-off between semantic segmentation
+                                and instance segmentation
                             </p>
                         </div>
                     </div>
