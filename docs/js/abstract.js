f2f3bb1206d15db0aa861fae781f3d7995a0a725
Christian Igel <igel@diku.dk>
2024-05-31 11:20:07 +0200

    Update abstract.js

--- docs/js/abstract.js
+++ docs/js/abstract.js
@@ -7,14 +7,12 @@ const Abstract = () => {
                         <h2 class="title is-3">Abstract</h2>
                         <div class="content has-text-justified">
                             <p>
-                                Low-quality housing is associated with an increased risk of mosquito-borne dis-
-                                eases. As certain roof characteristics are linked to an elevated risk of malaria,
+                                Low-quality housing is associated with an increased risk of mosquito-borne diseases. As certain roof characteristics are linked to an elevated risk of malaria,
                                 classification of roof types based on remote sensing imagery can support assess-
                                 ment of malaria risk and help preventing the disease. To support research in this
                                 area, we release the Nacala-Roof-Material dataset, which contains drone images
                                 from Mozambique with corresponding labels delineating houses and specifying
-                                their roof types. The dataset defines a multi-task computer vision problem, compris-
-                                ing object detection, classification, and segmentation. In addition, we benchmarked
+                                their roof types. The dataset defines a multi-task computer vision problem, comprising object detection, classification, and segmentation. In addition, we benchmarked
                                 different state-of-the-art approaches on the dataset. Canonical U-Nets, YOLOv8,
                                 and a custom decoder on pretrained DINOv2 served as baselines. We show that
                                 each of the methods has its advantages but none is superior on all tasks. While the
