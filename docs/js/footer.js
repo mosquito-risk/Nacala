46369dc696d067a18d766486c61c2e89b61b420b
Christian Igel <igel@diku.dk>
2024-06-01 14:10:24 +0200

    Update footer.js

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
                     <div class="column is-8">
                         <div class="content">
                             <p style="font-size: smaller">
-                                <small>This page was built using the <a href="https://github.com/eliahuhorwitz/Academic-project-page-template"
+                                This page was built using the <a href="https://github.com/eliahuhorwitz/Academic-project-page-template"
                                     target="_blank">Academic Project Page Template</a> which was adopted from the <a
                                         href="https://nerfies.github.io" target="_blank">Nerfies</a> project page.
                                 You are free to borrow the of this website, we just ask that you link back to this page in the footer.
@@ -14,7 +14,6 @@ const Footer = () => {
                                     href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative
                                     Commons Attribution-ShareAlike 4.0 International License</a>.
                             </p>
-
                         </div>
                     </div>
                 </div>
