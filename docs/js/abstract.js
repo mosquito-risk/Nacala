26f0dc403740c6e73249d37a738f24f32556acfa
Hui Zhang <omegazhanghui@gmail.com>
2024-06-05 18:32:50 +0200

    Update abstract

--- docs/js/abstract.js
+++ docs/js/abstract.js
@@ -7,24 +7,25 @@ const Abstract = () => {
                         <h2 class="title is-3">Abstract</h2>
                         <div class="content has-text-justified">
                             <p>
-                                Low-quality housing is associated with an increased risk of mosquito-borne diseases.
-                                As certain roof characteristics are linked to an elevated risk of malaria,
-                                classification of roof types based on remote sensing imagery can support assessment 
-                                of malaria risk and help preventing the disease. To support research in this
-                                area, we release the Nacala-Roof-Material dataset, which contains drone images
-                                from Mozambique with corresponding labels delineating houses and specifying
-                                their roof types. The dataset defines a multi-task computer vision problem, comprising 
-                                object detection, classification, and segmentation. In addition, we benchmarked
-                                different state-of-the-art approaches on the dataset. Canonical U-Nets, YOLOv8,
-                                and a custom decoder on pretrained DINOv2 served as baselines. We show that
-                                each of the methods has its advantages but none is superior on all tasks. While the
-                                tasks are closely related, accurate segmentation of objects need not imply accurate
+                                As low-quality housing and in particular certain roof characteristics are associated
+                                with an increased risk of malaria, classification of roof types based on remote
+                                sensing imagery can support the assessment of malaria risk and thereby help
+                                prevent the disease. To support research in this area, we release the Nacala-Roof-
+                                Material dataset, which contains high-resolution drone images from Mozambique
+                                with corresponding labels delineating houses and specifying their roof types. The
+                                dataset defines a multi-task computer vision problem, comprising object detection,
+                                classification, and segmentation. In addition, we benchmarked various state-of-the-
+                                art approaches on the dataset. Canonical U-Nets, YOLOv8, and a custom decoder
+                                on pretrained DINOv2 served as baselines. We show that each of the methods has
+                                its advantages but none is superior on all tasks, which highlights the potential of
+                                our dataset for future research in multi-task learning. While the tasks are closely
+                                related, accurate segmentation of objects does not necessarily imply accurate
                                 instance separation, and vice versa. We address this general issue by introducing a
-                                variant of the deep ordinal watershed (DOW) approach, where several output maps
-                                are generated allowing for accurate object delineation and separation. We show
-                                that DOW is a generic approach that improves the performance of both U-Net and
-                                DINOv2 backbones leading to a better trade-off between semantic segmentation
-                                and instance segmentation
+                                variant of the deep ordinal watershed (DOW) approach that additionally separates
+                                the interior of objects, allowing for improved object delineation and separation. We
+                                show that our DOW variant is a generic approach that improves the performance of
+                                both U-Net and DINOv2 backbones, leading to a better trade-off between semantic
+                                segmentation and instance segmentation.
                             </p>
                         </div>
                     </div>
